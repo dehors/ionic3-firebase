19f10881e780afd6fa45b13b969bacabc513e8eb
slam24 <luismartinez242011@gmail.com>
2017-08-28 03:13:49 -0600

    feat: Create with firebase

--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -3,7 +3,7 @@ import { AngularFireDatabase } from "angularfire2/database/database";
 
 @Injectable()
 export class NotesService{
-  constructor(public afBD: AngularFireDatabase){}
+  constructor(public afDB: AngularFireDatabase){}
   notes = [
     {id:1, title:'Note 1', description:'Description of the note 1'},
     {id:2, title:'Note 2', description:'Description of the note 2'},
@@ -16,7 +16,8 @@ export class NotesService{
     return this.notes.filter(function(e,i){return e.id == id})[0] || {id:null,title:null,description:null};
   }
   public createNote(note){
-    this.notes.push(note);
+    //this.notes.push(note);
+    this.afDB.database.ref('notes/'+note.id).set(note);
   }
   public editNote(note){
     for(let i = 0; i < this.notes.length; i++){
